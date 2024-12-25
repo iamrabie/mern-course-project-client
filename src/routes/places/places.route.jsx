b3c90a88609fe2f1a4ab6223a34de7d0ae06d077
Rabia Khan <106153809+iamrabie@users.noreply.github.com>
2024-12-25 11:31:12 +0500

    set up the routes of the app

---
+++ src/routes/places/places.route.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+
+const Places = () => {
+    return(
+        <>
+        <h1>Places</h1>
+        <Outlet />
+        </>
+    );
+}
+
+
+export default Places;
