410995317ef5e38ff3e4efc5f7de4f339a59c5cd
Rabia Khan <106153809+iamrabie@users.noreply.github.com>
2024-12-25 11:32:08 +0500

    added Browser Router and Routes and Route in index.js and app.js respectively.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { Routes , Route } from "react-router-dom";
+import Users from "./routes/users/users.route";
+import Places from "./routes/places/places.route";
+import NewPlace from "./routes/places/newPlace.route";
+
+
 function App() {
   return (
     <div className="App">
-      <h1>mern course project</h1>
+      <Routes>
+        <Route path="/" element={<Users />} />
+        <Route path="/places" element={<Places />}>
+          <Route path="new" element={<NewPlace />} /> 
+        </Route>
+        <Route path="*" element={<Users />} />
+      </Routes>
     </div>
   );
 }
